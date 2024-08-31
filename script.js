bb198d59b78ac5b1828d3ef8a6c5a63a43a79c57
Abdul Haseeb <haseeb1389446@gmail.com>
2024-08-30 22:59:20 -0700

    Mini project

---
+++ script.js
@@ -0,0 +1,104 @@
+// navbar
+
+document.querySelector(".navbar").innerHTML=`<nav>
+<div class="logo"><a href="index.html">BookParadise</a></div>
+<div class="navigation">
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="product.html">Product</a></li>
+        <li><a href="about.html">About us</a></li>
+        <li><a href="contect.html">Contect us</a></li>
+    </ul>
+</div>
+</nav>`
+
+
+// footer
+
+document.querySelector(".f").innerHTML=`<div class="row">
+<div class="f-col1"><a href="index.html" class="logowhite">BookParadise</a></div>
+<div class="f-col2">
+<a href="index.html" class="logowhite">BookParadise</a>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere repellendus incidunt eos ea, pariatur
+    debitis.</p>
+</div>
+<div class="f-col3">
+<h3>follow us</h3>
+<ul>
+    <li>facebook</li>
+    <li>twiter</li>
+    <li>instagram</li>
+    <li>youtube</li>
+</ul>
+</div>
+</div>
+
+<hr>
+
+<div class="footer">Copyright &copy; www.BookParadise.com | All right reseved!</div>`
+
+
+
+// pass hide & show
+
+function show(){
+    document.getElementById("pass").setAttribute("type","text");
+    document.getElementById("icon").setAttribute("class","fa-regular fa-eye-slash")
+}
+
+function hide(){
+    document.getElementById("pass").setAttribute("type","password");
+    document.getElementById("icon").setAttribute("class","fa-regular fa-eye")
+}
+
+
+// form vallidation
+
+function vallidation(){
+
+var name = document.getElementById("name").value ;
+var email = document.getElementById("email").value ;
+var pass = document.getElementById("pass").value ;
+var phone = document.getElementById("phone").value ;
+
+var namecheck = /^[A-Za-z ]{3,}$/;
+var emailcheck =  /^[A-Za-z0-9]{6,}@[A-Za-z]{2,}.[A-Za-z]{2,}$/;
+var passcheck =  /^[A-Za-z0-9]{8,}$/;
+var phonecheck = /^[0-9]{11}$/;
+
+
+
+if(name.match(namecheck)){
+    document.getElementById("nameerror").innerText = "**Yes, your right"
+    document.getElementById("nameerror").style.color = "green"
+}else{
+    document.getElementById("nameerror").innerText = "**No, something else!"
+    document.getElementById("nameerror").style.color = "red"
+}
+
+if(email.match(emailcheck)){
+    document.getElementById("emailerror").innerText = "**Yes, your right"
+    document.getElementById("emailerror").style.color = "green"
+}else{
+    document.getElementById("emailerror").innerText = "**No, something else!"
+    document.getElementById("emailerror").style.color = "red"
+}
+
+if(pass.match(passcheck)){
+    document.getElementById("passerror").innerText = "**Yes, your right"
+    document.getElementById("passerror").style.color = "green"
+}else{
+    document.getElementById("passerror").innerText = "**No, something else!"
+    document.getElementById("passerror").style.color = "red"
+}
+
+if(phone.match(phonecheck)){
+    document.getElementById("phoneerror").innerText = "**Yes, your right"
+    document.getElementById("phoneerror").style.color = "green"
+}else{
+    document.getElementById("phoneerror").innerText = "**No, something else!"
+    document.getElementById("phoneerror").style.color = "red"
+}
+
+
+}
